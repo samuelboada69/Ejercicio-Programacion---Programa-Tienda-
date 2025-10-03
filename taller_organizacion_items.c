e5e0ef3c1dbcff77bed298ea6c98fb9fc5596e8c
samuelboada69 <samuel.boada@udla.edu.ec>
2025-10-03 02:10:56 -0500

    Programa en C
    Subo mi primer programa en C

---
+++ taller_organizacion_items.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+int main(){
+
+    int id;
+    char nombre[50];
+    int stock;
+    float precio;
+    int opcion = 0;
+    int cantidad;
+    float ganancias = 0;
+
+    printf("Ingrese ID del producto: ");
+    scanf("%d",&id);
+
+    printf("Ingrese nombre del producto (una sola palabra): ");
+    scanf("%s",nombre);
+
+    printf("Ingrese stock inicial: ");
+    scanf("%d",&stock);
+
+    printf("Ingrese precio unitario: ");
+    scanf("%f",&precio);
+
+    while(opcion != 5){
+        printf("\n1. Vender\n");
+        printf("2. Reabastecer\n");
+        printf("3. Ver producto\n");
+        printf("4. Ver ganancias\n");
+        printf("5. Salir\n");
+        printf("Elija una opcion: ");
+        scanf("%d",&opcion);
+
+        if(opcion==1){
+            printf("Cantidad a vender: ");
+            scanf("%d",&cantidad);
+            if(cantidad<=stock){
+                stock = stock - cantidad;
+                ganancias = ganancias + (cantidad * precio);
+                printf("Venta realizada.\n");
+            } else {
+                printf("No hay suficiente stock.\n");
+            }
+        }
+
+        if(opcion==2){
+            printf("Cuantas unidades agrega: ");
+            scanf("%d",&cantidad);
+            stock = stock + cantidad;
+            printf("Stock actualizado.\n");
+        }
+
+        if(opcion==3){
+            printf("ID: %d\n",id);
+            printf("Nombre: %s\n",nombre);
+            printf("Stock: %d\n",stock);
+            printf("Precio: %.2f\n",precio);
+        }
+
+        if(opcion==4){
+            printf("Ganancias: %.2f\n",ganancias);
+        }
+
+        if(opcion==5){
+            printf("Saliendo...\n");
+        }
+    }
+
+    return 0;
+}
+
